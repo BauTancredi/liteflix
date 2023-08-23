ee011e9f15c7885dc9cab594d199223bcdc8c84c
bautancredi <bautistatancredi@hotmail.com>
2023-08-23 16:12:02 -0300

    create routes for fetching popular and featured movies

---
+++ src/app/api/featured/route.ts
@@ -0,0 +1,12 @@
+// GET
+// https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=6f26fd536dd6192ec8a57e94141f8b20"
+  ).then((res) => res.json());
+
+  return NextResponse.json({ res });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,71 @@
-export default function Home() {
-  return <main className="">Hello world!</main>;
+import Image from "next/image";
+
+const getPopularMovies = async () => {
+  const result = await fetch(process.env.URL + "/api/popular", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+  });
+
+  if (result.ok) {
+    return result.json();
+  }
+
+  return [];
+};
+
+const getFeaturedMovie = async () => {
+  const result = await fetch(process.env.URL + "/api/featured", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+  });
+
+  if (result.ok) {
+    return result.json();
+  }
+
+  return [];
+};
+
+interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  vote_average: number;
+  release_date: string;
+}
+
+// const MovieCard = ({ movie }: { movie: Movie }) => {
+//   return (
+//     <div>
+//       <Image
+//         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+//         alt={movie.title}
+//         width={100}
+//         height={100}
+//       />
+//     </div>
+//   );
+// };
+
+export default async function Home() {
+  const data = await getPopularMovies();
+  const data2 = await getFeaturedMovie();
+
+  const popularMovies: Movie[] = data.data.results.splice(0, 4);
+  const featuredMovie: Movie = data2.res.results[0];
+
+  return (
+    <main className="">
+      <ul>
+        {popularMovies.map((movie) => (
+          <li key={movie.id}>{/* <MovieCard movie={movie} /> */}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
