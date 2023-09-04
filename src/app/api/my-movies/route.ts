312b73fbc34f6da7aad00e8a6696807e7be2d52a
bautancredi <bautistatancredi@hotmail.com>
2023-09-03 23:13:47 -0300

    remove firebase import

--- src/app/api/my-movies/route.ts
+++ src/app/api/my-movies/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 // import firebase from "firebase/app";
-import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
